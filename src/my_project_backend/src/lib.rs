2f60495dcc81b1596bc0694fedd7ef18557eb99f
Marcel Reliszka <marcel.reliszka@wp.pl>
2024-12-03 15:37:25 +0000

    In progress to add blogs

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,23 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use std::cell::RefCell;
+
+mod blog;
+
+thread_local! {
+    static BLOGS: RefCell<Vec<String>> = RefCell::new(Vec::new());
 }
+
+#[ic_cdk::update]
+fn add_blog(new_blog: String) {
+    BLOGS.with(|blogs| blogs.borrow_mut().push(new_blog))
+}
+
+#[ic_cdk::query]
+fn get_blogs() -> Vec<String>  {
+    BLOGS.with(|blogs| blogs.borrow().clone())
+}
+
+// #[ic_cdk::query]
+// fn greet(name: String) -> String {
+//     format ("Hello, {}!", name)
+    // "greet": (text) -> (text) query;
+// }
