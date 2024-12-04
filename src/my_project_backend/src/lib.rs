3414a9427f13fc2de08ca086cf1ce0b6b4a91663
Marcel Reliszka <marcel.reliszka@wp.pl>
2024-12-04 17:17:11 +0000

    Added serde

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -12,6 +12,11 @@ thread_local! {
 }
 
 #[ic_cdk::update]
+fn add_config(new_config: Config) {
+    CONFIG.with(|config| *config.borrow_mut() = new_config)
+}
+
+#[ic_cdk::update]
 fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
     let config = CONFIG.with(|config| config.borrow().clone());
     if title.len() > config.max_title_len as usize {
@@ -21,8 +26,13 @@ fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, S
         return Err("Content is too long!".to_string())
     }
     if tags.len() > config.max_tags_count as usize {
-        return Err("Too many tags".to_string())
+        return Err("Too many tags!".to_string())
     }
+    let are_tags_in_config_tags = tags.iter().any(|tag| !config.tags.contains(tag));
+    if are_tags_in_config_tags {
+        return Err("Tags are not valid!".to_string())
+    }
+    
     let blog = Blog::new(title,content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
     let last_blog = BLOGS.with(|blogs| 
