ceaef2fb3524b1a0884e433a6ccd6eaf2f5bf1a1
Marcel Reliszka <marcel.reliszka@wp.pl>
2024-12-05 16:50:00 +0000

    Updated fe styles

--- src/my_project_frontend/src/App.vue
+++ src/my_project_frontend/src/App.vue
@@ -33,21 +33,32 @@ getBlogs();
     <img src="/logo2.svg" alt="DFINITY logo" class="mx-auto mt-4"/>
     <br />
     <br />
-    <form action="#" @submit="handleSubmit">
+    <form class="grid gap-4 pb-4 mb-4 border-solid border-b-2 border-sky-500" action="#" @submit="handleSubmit">
       <div>
-        <p class="text-white">Title:</p>
-        <input id="title" alt="title" type="text" />
+        <p class="text-black font-bold">Title:</p>
+        <input id="title" alt="title" type="text" class="w-full rounded-3xl py-1 px-4 outline-none border-solid border-2 border-black hover:border-indigo-500"/>
       </div>
       <div>
-        <p class="text-white">Content:</p>
-        <input id="content" alt="content" type="text" class="w-full"/>
+        <p class="text-black font-bold">Content:</p>
+        <textarea id="content" alt="content" type="text" class="w-full rounded-3xl py-1 px-4 outline-none min-h-[100px] border-solid border-2 border-black hover:border-indigo-500"/>
       </div>
       <div>
-        <p class="text-white">Tags:</p>
-        <input id="tags" alt="tags" type="text" />
+        <p class="text-black font-bold">Tags:</p>
+        <input id="tags" alt="tags" type="text" class="w-full rounded-3xl py-1 px-4 outline-none border-solid border-2 border-black hover:border-indigo-500"/>
       </div>
-      <button type="submit" class="text-white">Click to add!</button>
+      <div class="flex justify-end">
+        <button type="submit" class="text-white bg-indigo-500 rounded-3xl py-1 px-4">Click to add!</button>
+      </div>
     </form>
-    {{ blogs }}
+    <div>
+      <div v-for="blog in blogs">
+        <h3>{{ blog.title }}</h3>
+        <p>{{ blog.content }}</p>
+        <div>
+          {{ blog.date }}
+          {{ blog.tags }}
+        </div>
+      </div>
+    </div>
   </main>
 </template>
